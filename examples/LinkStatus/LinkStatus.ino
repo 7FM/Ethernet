080b2497c64d138b5ff3e2db7302409feedbc2ac
7FM <41307817+7FM@users.noreply.github.com>
2022-08-05 22:14:39 +0200

    autoformat

--- examples/LinkStatus/LinkStatus.ino
+++ examples/LinkStatus/LinkStatus.ino
@@ -11,34 +11,34 @@
   This example is public domain.
 */
 
-#include <SPI.h>
 #include <Ethernet.h>
+#include <SPI.h>
 
 void setup() {
-  // You can use Ethernet.init(pin) to configure the CS pin
-  //Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH Shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit FeatherWing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit FeatherWing Ethernet
+    // You can use Ethernet.init(pin) to configure the CS pin
+    // Ethernet.init(10);  // Most Arduino shields
+    // Ethernet.init(5);   // MKR ETH Shield
+    // Ethernet.init(0);   // Teensy 2.0
+    // Ethernet.init(20);  // Teensy++ 2.0
+    // Ethernet.init(15);  // ESP8266 with Adafruit FeatherWing Ethernet
+    // Ethernet.init(33);  // ESP32 with Adafruit FeatherWing Ethernet
 
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
-  auto link = Ethernet.linkStatus();
-  Serial.print("Link status: ");
-  switch (link) {
-    case Unknown:
-      Serial.println("Unknown");
-      break;
-    case LinkON:
-      Serial.println("ON");
-      break;
-    case LinkOFF:
-      Serial.println("OFF");
-      break;
-  }
-  delay(1000);
+    auto link = Ethernet.linkStatus();
+    Serial.print("Link status: ");
+    switch (link) {
+        case Unknown:
+            Serial.println("Unknown");
+            break;
+        case LinkON:
+            Serial.println("ON");
+            break;
+        case LinkOFF:
+            Serial.println("OFF");
+            break;
+    }
+    delay(1000);
 }
